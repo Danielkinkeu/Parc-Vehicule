9b00808789fb32977922574c19678af74886fdca
FLAVEL690 <115290303+FLAVEL690@users.noreply.github.com>
2023-12-04 10:49:20 +0100

    classe dépense

---
+++ vehicule_desktop/vehicule_desktop/Depense.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+
+[DebuggerDisplay($"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
+public class Depense
+{
+	public Depense()
+    {
+    public DateTime Date { get; set; }
+    public double Montant { get; set; }
+    public string Libelle { get; set; }
+
+    public Dépense(DateTime date, double montant, string libelle)
+    {
+        Date = date;
+        Montant = montant;
+        Libelle = libelle;
+    }
+
+    private string GetDebuggerDisplay()
+    {
+        return ToString();
+    }
+}
